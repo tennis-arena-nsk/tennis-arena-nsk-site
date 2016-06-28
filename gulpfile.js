dcdfa02b0d040a246f663dad6021bf4da51fbc65
deksden <deksden@deksden.com>
2016-06-28 19:36:22 +0600

    Gulp watching for file and live-reloading of browsers
    Just start default gulp task with: $ gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,21 @@
 const gulp = require('gulp');
 const clean = require('gulp-clean');
 const imagemin = require('gulp-imagemin');
-const webserver = require('gulp-server-livereload');
+const browserSync = require('browser-sync').create();
+
 const allGlob = '/**/*.*';
 
 // config vars for folders/files:
 var source = {  base: './source' };
 source.css = source.base + '/css';
-source.cssFiles = source.css + '/**/*.css';
+source.cssFiles = source.css + '/**/*.css'; // only css files inside source folder
 source.img = source.base + '/img';
-source.imgAll = source.img + allGlob;
+source.imgAll = source.img + allGlob; // all files in source img folder
 source.fonts = source.base + '/fonts';
 source.fontsAll = source.fonts + allGlob;
-source.htmlAll = source.base + '/**/*.html';
+source.htmlFiles = source.base + '/**/*.html'; // only html files inside source folder
 source.js = source.base + '/js';
-source.jsAll = source.js + '/**/*.js';
+source.jsFiles = source.js + '/**/*.js'; // only js files inside source folder
 
 var build = { base: './build' };
 build.css = build.base + '/css';
@@ -39,12 +40,18 @@ var task = {
   processHtml: 'process-html',
   processJs: 'process-js',
   processAll: 'process-build',
-  webserver: 'webserver'
+  webserver: 'webserver',
+  watchJs: 'watch-js',
+  watchCss: 'watch-css',
+  watchHtml: 'watch-html'
 };
 
 // define generic tasks
 gulp.task('default', [ task.webserver] );
-gulp.task( task.processAll, [ task.processImg, task.processFonts, task.processCss , task.processHtml, task.processJs ] );
+gulp.task( task.processAll, [ task.clean ], function() {
+  // use undocumented function to start complete build just after clean:
+  gulp.start( task.processImg, task.processFonts, task.processCss , task.processHtml, task.processJs );
+});
 
 
 // Clean: clear all files in build directory
@@ -54,17 +61,15 @@ gulp.task( task.clean, function () {
 });
 
 // CSS: process CSS files
-gulp.task( task.processCss, [task.clean], function () {
+gulp.task( task.processCss, function () {
   // plain copy of css files into dest:
   return gulp.src( source.cssFiles  )
     .pipe( gulp.dest( build.css ));
-
-  /* TODO: separate vendor csss files and own css files, vendor files should be already minified, just do some uncss - for own files miniy all */
 });
 
 
 // IMG: process image files: minify with imagemin
-gulp.task( task.processImg, [task.clean], function () {
+gulp.task( task.processImg, function () {
 
   // copy all files from img folder to dest
   return gulp.src( source.imgAll  )
@@ -74,32 +79,44 @@ gulp.task( task.processImg, [task.clean], function () {
 });
 
 // FONTS: process fonts:
-gulp.task( task.processFonts, [task.clean], function () {
+gulp.task( task.processFonts, function () {
   // plain copy of all font files into dest:
   return gulp.src( source.fontsAll  )
     .pipe( gulp.dest( build.fonts ));
 });
 
 // HTML: process pages
-gulp.task( task.processHtml, [task.clean], function () {
+gulp.task( task.processHtml, function () {
   // plain copy:
-  return gulp.src( source.htmlAll  )
+  return gulp.src( source.htmlFiles  )
     .pipe( gulp.dest( build.base ));
 });
 
 // JS: process js
-gulp.task( task.processJs, [task.clean], function () {
+gulp.task( task.processJs, function () {
   // plain copy:
-  return gulp.src( source.jsAll  )
+  return gulp.src( source.jsFiles  )
     .pipe( gulp.dest( build.js ));
 });
 
-// Start webserver with live-reload (end with Ctrl+c)
+// Watch-related tasks: reload dev browser after processing files
+gulp.task( task.watchJs, [ task.processJs ], browserSync.reload);
+gulp.task( task.watchHtml, [ task.processHtml ], browserSync.reload);
+gulp.task( task.watchCss, [ task.processCss ], browserSync.reload);
+
+// Start webserver with live reloading:
 gulp.task(task.webserver, [task.processAll], function() {
-  return gulp.src( build.base )
-    .pipe(webserver({
-      livereload: true,
-      directoryListing: false,
-      open: true
-    }));
+  // start webserver with live reloading feature:
+  browserSync.init({
+    server: {
+      baseDir: build.base
+    }
+  });
+
+  // add browserSync.reload to the tasks array to make
+  // all browsers reload after tasks are complete.
+  gulp.watch( source.jsFiles, [ task.watchJs ] );
+  gulp.watch( source.htmlFiles, [ task.watchHtml ] );
+  gulp.watch( source.cssFiles, [ task.watchCss ] );
 });
+
